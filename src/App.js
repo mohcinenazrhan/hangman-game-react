72dab9cc0eb7448049111ab2c411eddb28a16181
Mohcine NAZRHANE <mohcine@gmail.com>
2018-08-26 18:48:21 +0000

    add help and draw progress

--- src/App.js
+++ src/App.js
@@ -11,16 +11,23 @@ class App extends Component {
       expression: [],
       nbrTry: 0,
       isCompleted: false,
-      score: 0
+      score: 0,
+      stillTry: 0,
+      help: 0
     }
     this.alphabets = null
     this.expressions = null
+    this.currentExpression = null
     this.historyExps = []
+    this.progressDraw = 0
+    this.messageState = null
+    this.drawSteps = 6
+    this.progressDrawStep = 1
   }
 
   componentWillMount(){
     this.alphabets = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
-
+    // this.expressions = ['Jansfsdfsdfs']
     this.expressions = ['January', 'April', 'March', 'Websites', 'Devices', 'How', 'Mobile',
       "NOMBRE", "GEANTE", "CORAUX", "ROULEAU", "EJECTER", "LIVRETS",
       "DIVISION", "LICORNES", "FOURNEAU", "EMPLETTE", "CLEPSYDRE", "INDIGENES",
@@ -44,13 +51,22 @@ class App extends Component {
     return this.alphabets.map((letter) => {
       return {
         letter: letter,
-        disabled: false
+        disabled: false,
+        correct: null
       }
     })
   }
 
   // Arrow fx for binding
   newGame = () => {
+
+    this.messageState = 'default'
+    this.progressDraw = 0
+    if (this.expressions.length === 0) {
+      this.expressions = this.historyExps
+      this.historyExps = []
+    }
+
     // get random expression
     const randExpression = this.expressions[Math.floor(Math.random() * this.expressions.length)];
     // save random expression
@@ -58,13 +74,23 @@ class App extends Component {
     // remove random expression from expressions array to avoid choose it again
     this.expressions = this.expressions.filter((value) => value !== randExpression)
 
+    this.currentExpression = randExpression
     // split expression as array of objects letters + states for better handling
     const expression = randExpression.toUpperCase().split('').map((letter) => {
       return {
         letter: letter,
-        hidden: letter === ' ' ? false : true
+        hidden: letter === ' ' ? false : true,
+        class: letter === ' ' ? 'space' : 'lettre-hide',
+        style: { color: '#FFF' }
       }
     })
+
+    // const stillTry = expression.length + 2
+    const stillTry = Math.floor(expression.length / 2)
+    // this.progressDraw = stillTry < drawSteps ? drawSteps - stillTry : 0
+    this.progressDrawStep = stillTry < this.drawSteps ? 1 : Math.floor(stillTry / this.drawSteps)
+
+    const help = Math.floor(expression.length / 3)
 
     // Update the state
     this.setState({
@@ -72,37 +98,75 @@ class App extends Component {
       expression,
       nbrTry: 0,
       isCompleted: false,
-      score: 0
+      score: 0,
+      stillTry,
+      help
     })
   }
 
-
   letterClick(letter) {  
-    // default score  
-    let score = -1
-
-    // Disable the clicked button 
-    const letters = this.state.letters.map( (row) => {
-      if (letter === row.letter) row.disabled = true
-      return row
-    })
+    // default value
+    let correctLetter = false,
+        isCompleted = false
 
     // Show letter found
-    const expression = this.state.expression.map((row) => {
+    const expression = this.state.expression.map( (row) => {
       if (letter === row.letter) {
         row.hidden = false
-        score = 2
+        correctLetter = true
       }
       return row
     })
+
+    // Disable the clicked button 
+    const letters = this.state.letters.map( (row) => {
+      if (letter === row.letter) {
+        row.disabled = true 
+        row.correct = correctLetter
+      }
+      return row
+    })
+
+    // score
+    const score = correctLetter ? 2 : -1
+
+    let nbrTry = this.state.nbrTry
+
+    if (this.isCompleted(expression)) {
+      isCompleted = true
+      this.messageState = 'succes'
+    }
+    else if (!correctLetter) {
+      nbrTry = this.state.nbrTry + 1
+      
+      if (nbrTry >= this.state.stillTry) { 
+        this.progressDraw = this.drawSteps; 
+        isCompleted = true
+        this.messageState = 'failed'
+        this.showExpression()
+      } else if (nbrTry % this.progressDrawStep === 0 && this.progressDraw + 1 < this.drawSteps) this.progressDraw++;
+      // && this.state.stillTry < (this.progressDrawStep * this.drawSteps) + nbrTry
+    }
 
     // Update the state
     this.setState({
       letters,
       expression,
-      nbrTry: this.state.nbrTry + 1,
+      nbrTry,
       score: this.state.score + score,
-      isCompleted: this.isCompleted(expression) // Check if the expression is completely found
+      isCompleted
+      // isCompleted: this.isCompleted(expression) // Check if the expression is completely found
+    })
+  }
+
+  //#d40012
+  showExpression(){
+    return this.state.expression.map((row) => {
+      if (row.hidden) {
+        row.style = { color:'#d40012' }
+        row.hidden = false
+      }
+      return row
     })
   }
 
@@ -124,12 +188,14 @@ class App extends Component {
    * return JXS code : boutons du clavier
    */
   getKeyboard(){
-    return this.state.letters.map((row, index) => (
-      <button className={row.disabled === false ? 'btn btn-letter' : 'btn btn-letter btn-disabled'}
+    return this.state.letters.map((row, index) => {
+      const className = 'btn btn-letter ' + (row.disabled ? 'btn-disabled ' : '') + (row.correct === null ? '' : (row.correct ? 'btn-correct' : 'btn-wrong'))
+      return <button 
+        className={className}
         key={index}
         disabled={row.disabled}
         onClick={() => this.letterClick(row.letter)} >{row.letter}</button>
-    ))
+    })
   }
 
   /**
@@ -139,18 +205,74 @@ class App extends Component {
     return <button className="btn btn-control" onClick={this.newGame}> REJOUER </button >
   }
 
+  // like vuejs computed fucntion 
+  getScore(){
+    return this.state.score + 2
+  }
+
+  getHelp() {
+    let expression = this.state.expression.filter(row => row.hidden)
+    const randRow = expression[Math.floor(Math.random() * expression.length)]
+
+    expression = this.state.expression.map((row) => {
+      if (row === randRow) row.hidden = false
+      return row
+    })
+
+    // console.log(expression);
+
+    // let first = true
+    // const expression = this.state.expression.map((row) => {
+    //   if (row.hidden && first) {
+    //     row.hidden = false
+    //     first = false
+    //   }
+    //   return row
+    // })
+
+    this.setState({
+      score: this.state.score - 1,
+      help: this.state.help - 1,
+      expression
+    })
+  }
+
+  getProgressDwar(){
+    return {
+      backgroundPosition: `${this.progressDraw * -200}px`
+    }
+  }
+
+  message() {
+    switch (this.messageState) {
+      case 'failed':
+        return `Malheureusement, vous avez échoué, l'expression c'était: ${this.currentExpression}`;
+      case 'succes':
+        return 'Bravo, vous avez réussi';
+      default:
+        return 'Bon courage, que la force soit avec vous.';
+    }
+  }
+
   render() {
-    const { nbrTry, score, expression, isCompleted } = this.state
+    const { help, stillTry, nbrTry, score, expression, isCompleted } = this.state
     return (
       <div className="App">
         <div className="head-container">
-          <p> Nombre d'essais : { nbrTry } </p>
-          <p> Votre score : { score } </p>
+          <button disabled={!help} className={(!help ? 'stop-help' : '') + ' btn btn-control' } onClick={() => this.getHelp()}>Help = -1 score</button>
+          <p> Nombre d'essais : {nbrTry} </p>
+          <p> You have {stillTry} wrong attempt </p>
+          <p> Votre score : {score} </p>
+          {/* <p> test score : { this.getScore() } </p> */}
+        </div>
+        <div className="draw-container">
+          <div className="draw-img-progress" style={this.getProgressDwar()}>
+          </div>
         </div>
         <div className="word-container">
           {
             expression.map((row, index) => (
-              <div className={row.letter === ' ' ? 'space' : 'lettre-hide'}
+              <div style={row.style} className={row.class}
                    key={index}>{ row.hidden === true ? '_' : row.letter }</div>
             ))
           }
@@ -160,7 +282,8 @@ class App extends Component {
             isCompleted === true ? this.getBtnNewGame() : this.getKeyboard()
           }
         </div>
-        <p>{ isCompleted === true ? 'Bravo, vous avez réussi' : 'Bon courage, que la force soit avec vous.'}</p>
+        { /* <p>{ isCompleted === true ? 'Bravo, vous avez réussi' : 'Bon courage, que la force soit avec vous.'}</p> */ }
+        <p>{ this.message() }</p>
       </div>
     );
   }
